cd026da57f09cc6c6626b2a816223e1a90425ce0
Paul Fortin <paul@cp-soft.com>
2019-11-22 06:33:36 -0500

    Tutorial done

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Fetch from "./Fetch.svelte";
+	import Form from "./Form.svelte";
+	const props = {
+    url: "https://jsonplaceholder.typicode.com/todos" //,
+    // title: "A list of todos"
+    };
+  let searchTerm;
+
+  function handleSubmit() {
+    const { value } = this.elements.search;
+    searchTerm = value;
+  }
+</script>
+
+<Form {handleSubmit}/>
+
+<Fetch {searchTerm} let:data>
+  <h1>A list of todos</h1>
+  <ul>
+    {#each data as link}
+      <li><a href={link.url}>{link.title}</a></li>
+    {/each}
+  </ul>
+</Fetch>
